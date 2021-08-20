688ce453370d36aa2ea6890b2d42fc2d5b9c7838
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-20 20:06:16 +0800

    trailer

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,7 +12,20 @@ function HeroSection() {
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
             <div className="hero-btns"></div>
-                <Button className="btns" buttonSyle="btn--outline">GET STARTED</Button>
+                <Button 
+                    className="btns" 
+                    buttonSyle="btn--outline"
+                    buttonSize='btn--large'
+                >
+                    GET STARTED
+                </Button>
+                <Button 
+                    className="btns" 
+                    buttonSyle="btn--primary"
+                    buttonSize='btn--large'
+                >
+                    WATCH TRAILER <i className='far fa-play-circle' />
+                </Button>
 
         </div>
     )
