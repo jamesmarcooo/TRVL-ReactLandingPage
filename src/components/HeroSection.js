77fa850711c5aa375970670be8fd2df715973ce3
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-20 19:54:48 +0800

    import

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Button } from './Button';
+import '../App.css';
+import './HeroSection.css';
+
+
 
 function HeroSection() {
     return (
