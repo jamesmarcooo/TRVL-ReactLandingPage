591e9e1225bf4d2b98e304ee34bf55c8b28521a8
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-23 17:19:18 +0800

    products

---
+++ src/components/pages/Services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+
+// export default function Services() {
+//   return <h1 className='services'>SERVICES</h1>;
+// }
+
+function Services() {
+  return (
+    <div className='services'>
+      <img className='img-s' src="images/img-2.jpg" alt="services-bg"></img>
+      <h1>SERVICES</h1>
+    </div>
+  )
+}
+
+export default Services
