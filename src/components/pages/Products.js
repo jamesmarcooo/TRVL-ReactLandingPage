ec89d10d8c63d06d26a17fec90cd3f332a2f7517
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-23 17:18:51 +0800

    services

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../../App.css';
 
-export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
+
+function Products() {
+  return (
+    <div className='products'>
+      <img className='img-p' src='images/img-1.jpg' alt="products-bg"></img>
+      <h1>PRODUCTS</h1>
+    </div>
+  )
 }
+
+export default Products
