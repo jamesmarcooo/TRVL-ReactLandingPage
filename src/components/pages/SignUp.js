5772b6f3f83a851d3d38ae0bdeccfc154380156a
Marco <james.marco.reyes@eee.upd.edu.ph>
2021-08-23 17:19:34 +0800

    signup

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+
+
+function SignUp() {
+  return (
+    <div className='sign-up'>
+      <img className='img-su' src='images/img-8.jpg' alt="products-bg"></img>
+      <h1>LIKE & SUBSCRIBE</h1>
+    </div>
+  )
+}
+
+export default SignUp
